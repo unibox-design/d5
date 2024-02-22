9f58d09307b06d736be50b794e50f4d97cfd13fd
Proximity <proximity@Proximitys-MacBook-Pro.local>
2024-02-22 18:18:06 +0530

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+// App.js
+
+import React from 'react';
+import './App.css'; 
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <a href="/">Home</a>
+        <a href="/games">Games</a>
+      </nav>
+    
+      <h1>GameHost</h1>
+      <p>Host and play games online!</p>
+    </div>
+  );
+}
+
+export default App;
